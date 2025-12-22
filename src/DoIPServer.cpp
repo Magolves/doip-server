4a4cc85568a2fc97d50c2fb90501026d95588cc1
Oliver Wieland <oliver.wieland@unitybox.de>
2025-12-22 13:57:55 +0100

    fix: Logger init

--- src/DoIPServer.cpp
+++ src/DoIPServer.cpp
@@ -25,11 +25,8 @@ DoIPServer::~DoIPServer() {
 }
 
 DoIPServer::DoIPServer(const ServerConfig &config)
-    : m_config(config) {
+    : m_config(config), m_doipLog(Logger::get("server")), m_udpLog(Logger::getUdp()), m_tcpLog(Logger::getTcp()) {
 
-    m_doipLog = Logger::get("server");
-    m_udpLog = Logger::getUdp();
-    m_tcpLog = Logger::getTcp();
 
     setLoopbackMode(m_config.loopback);
 }
