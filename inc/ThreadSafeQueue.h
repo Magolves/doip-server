7bc6be4b810b4ccf4d2dafef504f7c014cd52f76
Oliver Wieland <oliver.wieland@unitybox.de>
2025-12-24 14:18:30 +0100

    fix: Fix readme comment #7 (templated push for perfect forwarding)

--- inc/ThreadSafeQueue.h
+++ inc/ThreadSafeQueue.h
@@ -1,38 +1,38 @@
 #pragma once
 
-#include <queue>
-#include <mutex>
-#include <condition_variable>
 #include <chrono>
+#include <condition_variable>
+#include <mutex>
+#include <queue>
 
-template<typename T>
+template <typename T>
 class ThreadSafeQueue {
-private:
+  private:
     std::queue<T> queue_;
     mutable std::mutex mutex_;
     std::condition_variable cv_;
     bool stopped_ = false;
 
-public:
+  public:
     ThreadSafeQueue() = default;
-    
+
     ~ThreadSafeQueue() {
         stop();
     }
-    
+
     // Disable copy operations
-    ThreadSafeQueue(const ThreadSafeQueue&) = delete;
-    ThreadSafeQueue& operator=(const ThreadSafeQueue&) = delete;
-    
+    ThreadSafeQueue(const ThreadSafeQueue &) = delete;
+    ThreadSafeQueue &operator=(const ThreadSafeQueue &) = delete;
+
     // Enable move operations
-    ThreadSafeQueue(ThreadSafeQueue&& other) noexcept {
+    ThreadSafeQueue(ThreadSafeQueue &&other) noexcept {
         std::lock_guard<std::mutex> lock(other.mutex_);
         queue_ = std::move(other.queue_);
         stopped_ = other.stopped_;
         other.stopped_ = true;
     }
-    
-    ThreadSafeQueue& operator=(ThreadSafeQueue&& other) noexcept {
+
+    ThreadSafeQueue &operator=(ThreadSafeQueue &&other) noexcept {
         if (this != &other) {
             std::scoped_lock lock(mutex_, other.mutex_);
             queue_ = std::move(other.queue_);
@@ -43,21 +43,29 @@ public:
         return *this;
     }
 
-    void push(T item) {
+    /**
+     * @brief Push an item into the queue.
+     *
+     * @tparam U Type of the item to push
+     * @param item Item to push into the queue
+     */
+    template <typename U>
+    void push(U&& item) {
         {
             std::lock_guard<std::mutex> lock(mutex_);
-            if (stopped_) return;
-            queue_.push(std::move(item));
+            if (stopped_)
+                return;
+            queue_.push(std::forward<U>(item));
         }
         cv_.notify_one();
     }
 
-    bool pop(T& item, std::chrono::milliseconds timeout = std::chrono::milliseconds(100)) {
+    bool pop(T &item, std::chrono::milliseconds timeout = std::chrono::milliseconds(100)) {
         std::unique_lock<std::mutex> lock(mutex_);
 
         if (!cv_.wait_for(lock, timeout, [this] {
-            return !queue_.empty() || stopped_;
-        })) {
+                return !queue_.empty() || stopped_;
+            })) {
             return false; // Timeout
         }
 
